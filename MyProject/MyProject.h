81fc7e1b9320268eccfdf49949277c9007c98e46
Quanlong He <kyan.ql.he@gmail.com>
2015-08-27 17:29:15 +0800

    Initial commit

---
+++ MyProject/MyProject.h
@@ -0,0 +1,19 @@
+//
+//  MyProject.h
+//  MyProject
+//
+//  Created by generator-swift-framework.
+//  Copyright © 2015 example-org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyProject.
+FOUNDATION_EXPORT double MyProjectVersionNumber;
+
+//! Project version string for MyProject.
+FOUNDATION_EXPORT const unsigned char MyProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyProject/PublicHeader.h>
+
+
---
+++ MyProject/MyProject.swift
@@ -0,0 +1,23 @@
+//
+//  MyProject.swift
+//  MyProject
+//
+//  Copyright © 2015 example-org. All rights reserved.
+//
+
+import UIKit
+
+public class MyProject {
+    
+    // MARK: Properties
+    
+    private var someProperty: String
+    
+    // MARK: Initilizers
+    
+    public init() {
+        someProperty = "s"
+    }
+}
+
+
